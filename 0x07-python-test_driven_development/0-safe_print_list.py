a51bf013f723c758ae96cd25693a3d5f04dc5a5c
freedompraise <dikepraise119@gmail.com>
2022-10-13 12:45:36 -0700

    exceptions 001

---
+++ 0x07-python-test_driven_development/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = int()
+    for i in x:
+        try:
+            print(my_list[i],end ="")
+            count += 1 
+        except:
+            break
+
+    return count
